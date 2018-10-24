177c85054e9a5f6a26cbcc3afa5c48eaeac29d83
moospit <moospit@e1nself.de>
2018-10-24 17:34:48 +0200

    minimal config parsing

--- main.py
+++ main.py
@@ -20,10 +20,25 @@ def main() -> None:
     print(f"Parsing config")
     config = load_config()
     print(config)
+
+    targets = config.get('targets')
+    addr_sbc = targets.get('sbc')
+    addr_codec = targets.get('codec')
+    print(f"SBC: {addr_sbc}, Codec: {addr_codec}")
+    # addr_sbc = config.get('targets').get('sbc')
+    # addr_codec = config.get('targets').get('codec')
     
-    print(f"Route Base Check: {plugins.base_check('172.31.10.13')}")
-    print(f"Local Base Check: {plugins.base_check('192.168.23.10')}")
-    print(f"Ping: {plugins.ping('172.31.10.13')}")  # deprecated
+    for module, tests in config.get('modules').items():
+        print(module, tests)
+        for test in tests:
+            if test in dir(plugins):
+                func = getattr(plugins, test)
+                print(func, func.__name__)
+
+    
+    # print(f"Route Base Check: {plugins.base_check('172.31.10.13')}")
+    # print(f"Local Base Check: {plugins.base_check('192.168.23.10')}")
+    # print(f"Ping: {plugins.ping('172.31.10.13')}")  # deprecated
 
 
 if __name__ == "__main__":
